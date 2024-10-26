fda5ca96cd712577102cd926867e2b84667630b0
CleomadsonMedeiros <cleomadsonmedeiros@gmail.com>
2024-10-26 14:02:00 -0300

    Adicionada função de editar arquivo

--- Editor.asm
+++ Editor.asm
@@ -3,33 +3,27 @@ msg_bem_vindo: .asciiz "Bem-vindo ao editor de texto MIPS - Use ':' para salvar 
 msg_buffer_cheio: .asciiz "\nErro: Buffer está cheio\n"
 msg_erro: .asciiz "\nErro ao abrir o arquivo"
 msg_comandos: .asciiz "\nSalvar - s | Cancelar - (qualquer tecla)\n"
-msg_arquivo: .asciiz "\nDigite o nome o arquivo + extensão - Confirme com ':'\n"
+msg_arquivo: .asciiz "\nDigite o nome o arquivo + extensão - Confirme com 'Enter'\n"
 msg_cancelar: .asciiz "\nOperação cancelada\n"
 quebra_linha: .asciiz "\n"
 buffer: .space 50 #Caso queira alterar o tamanho do buffer, terá que alterar o limite do contador também
 nome_arquivo: .space 256
 
 .text
-#Mudanças a serem feitas:
-#Implementar procedimento para abrir um arquivo e edita-lo.
-#{
-#- Escrever no buffer todo conteúdo do arquivo.
-#- Usar a mesma lógica de adição/remoção de caracteres.
-#- Usar o mesmo nome do arquivo para salvar(sobrescrever).
-#}
-
 #Legenda:
 #$t0 == buffer
 #$t1 == contador do buffer
 #$t2 == caractere lido (Uso Geral)
 #$t3 == Descritor
 #$t4 == buffer nome do arquivo
+#$t5 == Caractere nulo
+#$t6 == Flag para salvar/editar
 
-# Inicialização dos buffers + contador de caracteres.
+# Inicialização dos buffers + contador de caracteres + flag.
 la $t0, buffer
 la $t4, nome_arquivo
 li $t1, 0
-
+li $t6, 0
 #Syscall para imprimir a mensagem de bem-vindo.
 li $v0, 4
 la $a0, msg_bem_vindo
@@ -43,14 +37,13 @@ li $v0, 12
 syscall
 move $t2, $v0
 
-#Verifica se a entrada do usuário é "Hífen" (ASCII - 45 -> "-");
+#Verifica se a entrada do usuário é "-" (ASCII - 45).
+beq $t2, 45, apagar
 
-beq $t2, 45, backspace
-
-#Verifica se a entrada do usuário ($t2) é igual a ":"(ASCII - 58).
+#Verifica se a entrada do usuário é ":"(ASCII - 58).
 beq $t2, 58, detectado_dois_pontos
 
-#Verifica se o buffer está cheio - Contador ($t1) - Máximo Caracteres (50)
+#Verifica se o buffer está cheio - Contador ($t1) - Máximo Caracteres (50).
 bne $t1, 50, continuacao
 
 #Imprime mensagem que o buffer está cheio e volta para o loop de entrada.
@@ -66,18 +59,19 @@ addi $t0, $t0, 1
 addi $t1, $t1, 1
 j loop_entrada
 
-# Função para tratar o "Backspace"
-backspace:
+# Função para apagar
+apagar:
 # Verifica se o buffer não está vazio
-ble $t1, 0, loop_entrada
+beq $t1, 0, tratar_apagar
 
 # Decrementa o contador e ajusta o ponteiro do buffer para apagar o último caractere
 li $t5, 0
 sb $t5, 0($t0)
+addi $t1, $t1, -1
 addi $t0, $t0, -1
 sb $t5, 0($t0)
-addi $t1, $t1, -1
 
+tratar_apagar:
 li $v0, 4
 la $a0, quebra_linha
 syscall
@@ -102,16 +96,32 @@ li $v0, 12
 syscall
 move $t2, $v0
 
-#Verifica se o usuário digitou "s" após ":" para Salvar o arquivo.
+#Verifica se o usuário digitou "s/S" após ":" para Salvar o arquivo.
 beq $t2, 115, salvar_arquivo
+beq $t2, 83, salvar_arquivo
 
-#Caso contrário, pula novamente para o loop de entrada.
+#Verifica se o usuário digitou "e/E" após ":" para editar o arquivo.
+beq $t2, 101, editar_arquivo
+beq $t2, 69, editar_arquivo
+
+#Verifica se o usuário digitou "c/C" após ":" para editar o arquivo.
+beq $t2, 99, cancelar
+beq $t2, 67, cancelar
+
+j detectado_dois_pontos
+
+cancelar:
 li $v0, 4
 la $a0, msg_cancelar
 syscall
 j loop_entrada
 
+editar_arquivo:
+li $t6, 1 #1 para o flag de escrita
+li $t1, 0
+
 salvar_arquivo:
+beq $t6, 2, escrita_arquivo
 #Imprime para o usuário digitar o nome do arquivo.
 li $v0, 4
 la $a0, msg_arquivo
@@ -123,8 +133,8 @@ li $v0, 12
 syscall
 move $t2, $v0
 
-#Verifica se a entrada do usuário ($t2) é igual a ":"(ASCII - 58).
-beq $t2, 58, detectado_dois_pontos_arquivo
+#Verifica se a entrada do usuário ($t2) é igual a "Enter"(ASCII - 10).
+beq $t2, 10, detectado_enter
 
 #Armazena o caractere em $t4(buffer de arquivo), vai para a próxima célula de memória.
 sb $t2, 0($t4)
@@ -132,11 +142,12 @@ addi $t4, $t4, 1
 
 j loop_entrada_arquivo
 
-detectado_dois_pontos_arquivo:
+detectado_enter:
+beq $t6, 0, escrita_arquivo
 #Abre o arquivo
 li $v0, 13
 la $a0, nome_arquivo
-li $a1, 1
+li $a1, 0 #Leitura
 syscall
 
 #Erro ao abrir o arquivo
@@ -145,7 +156,67 @@ blt $v0, 0, erro
 #Guarda o descritor do arquivo em $t3
 move $t3, $v0
 
-#Syscall para escrita em um arquivo
+#Syscall para edição do arquivo:
+contar_caractere:
+li $v0, 14
+move $a0, $t3
+la $a1, buffer
+li $a2, 1
+syscall
+
+bgt $v0, 0, leu_caractere
+j terminada_contagem
+
+leu_caractere:
+addi $t1, $t1, 1
+addi $t0, $t0, 1
+j contar_caractere
+
+terminada_contagem:
+li $v0, 16
+move $a0, $t3
+syscall
+
+li $v0, 13
+la $a0, nome_arquivo
+li $a1, 0 #Leitura
+syscall
+move $t3, $v0
+
+li $v0, 14
+move $a0, $t3
+la $a1, buffer
+move $a2, $t1
+syscall
+
+li $v0, 16
+move $a0, $t3
+syscall
+
+li $t6, 2
+
+li $v0, 4
+la $a0, buffer
+syscall
+j loop_entrada
+
+escrita_arquivo:
+li $v0, 13
+la $a0, nome_arquivo
+li $a1, 1 #Escrita
+syscall
+move $t3, $v0
+
+blt $v0, 0, erro
+
+li $v0, 4
+la $a0, buffer
+syscall
+
+li $v0, 1
+move $a0, $t1
+syscall
+
 li $v0, 15
 move $a0, $t3
 la $a1, buffer
